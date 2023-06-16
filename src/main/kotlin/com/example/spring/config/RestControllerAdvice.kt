9f70bd0a6dad40f26c3cef3ad5d6688b08473abf
hunkicho <jgchk4814@gmail.com>
2023-06-16 16:03:30 +0900

    feat(exception): Add exception control
    - Control methodArgumentNotValidException

--- src/main/kotlin/com/example/spring/config/RestControllerAdvice.kt
+++ src/main/kotlin/com/example/spring/config/RestControllerAdvice.kt
@@ -5,13 +5,38 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.lang.RuntimeException
+
 
 @RestControllerAdvice
 class ControllerAdvice {
     protected val log: Logger = LoggerFactory.getLogger(this::class.simpleName)
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<Any> {
+        val bindingResult: BindingResult = ex.bindingResult
+        val builder = StringBuilder()
+        for (fieldError in bindingResult.fieldErrors) {
+            builder.append("[")
+            builder.append(fieldError.field)
+            builder.append("](은)는 ")
+            builder.append(fieldError.defaultMessage)
+            builder.append(" 입력된 값: [")
+            builder.append(fieldError.rejectedValue)
+            builder.append("]")
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(
+                BaseResponseException(
+                    ErrorCode.INVALID_PARAMETER,
+                    builder.toString()
+                )
+            )
+    }
 
     @ExceptionHandler(NoDataException::class)
     fun noDataException(ex: NoDataException): ResponseEntity<Any> {
