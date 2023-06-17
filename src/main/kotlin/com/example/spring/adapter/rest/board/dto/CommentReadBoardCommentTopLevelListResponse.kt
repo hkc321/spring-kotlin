10545fffcfc1e0793222cd00b0ad55cc7e1108cc
hunkicho <jgchk4814@gmail.com>
2023-06-17 14:50:04 +0900

    refactor(file): Delete unused file
    - Delete unused file

--- src/main/kotlin/com/example/spring/adapter/rest/board/dto/CommentReadBoardCommentTopLevelListResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.spring.adapter.rest.board.dto
-
-import com.example.spring.domain.board.Comment
-
-data class CommentReadBoardCommentTopLevelListResponse(
-    val commentList: List<Comment>,
-    val islast: Boolean,
-    val isEmpty: Boolean
-)
