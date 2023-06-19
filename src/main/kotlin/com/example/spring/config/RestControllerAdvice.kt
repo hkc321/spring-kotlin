5cd6a9b73d9186c96cece585b9498a881dd29d5d
hunkicho <jgchk4814@gmail.com>
2023-06-19 23:05:36 +0900

    feat(exception): Add exception control
    - Add exception control that MethodArgumentTypeMismatchException

--- src/main/kotlin/com/example/spring/config/RestControllerAdvice.kt
+++ src/main/kotlin/com/example/spring/config/RestControllerAdvice.kt
@@ -5,6 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -15,6 +16,17 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @RestControllerAdvice
 class ControllerAdvice {
     protected val log: Logger = LoggerFactory.getLogger(this::class.simpleName)
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun httpMessageNotReadableException(ex: HttpMessageNotReadableException): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(
+                BaseResponseException(
+                    ErrorCode.INVALID_PARAMETER,
+                    "파라미터를 확인해주세요."
+                )
+            )
+    }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException::class)
     fun methodArgumentTypeMismatchException(ex: MethodArgumentTypeMismatchException): ResponseEntity<Any> {
