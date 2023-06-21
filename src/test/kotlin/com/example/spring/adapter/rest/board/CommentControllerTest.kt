b759396bd28bd59ff64f523a150242e9c4aa4b35
hunkicho <jgchk4814@gmail.com>
2023-06-21 22:50:30 +0900

    test(comment): Add test code
    - Add test code that comment controller

---
+++ src/test/kotlin/com/example/spring/adapter/rest/board/CommentControllerTest.kt
@@ -0,0 +1,28 @@
+package com.example.spring.adapter.rest.board
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CommentControllerTest {
+
+    @Test
+    fun createComment() {
+    }
+
+    @Test
+    fun readComment() {
+    }
+
+    @Test
+    fun readChildComment() {
+    }
+
+    @Test
+    fun updateComment() {
+    }
+
+    @Test
+    fun deleteComment() {
+    }
+}
