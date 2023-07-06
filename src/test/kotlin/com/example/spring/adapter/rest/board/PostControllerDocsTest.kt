e398cfc46196d5b576eb63d2727f8ebf54703d4e
hunkicho <jgchk4814@gmail.com>
2023-07-06 15:10:55 +0900

    test(post): Add test code
    - Add test code about post

---
+++ src/test/kotlin/com/example/spring/adapter/rest/board/PostControllerDocsTest.kt
@@ -0,0 +1,29 @@
+package com.example.spring.adapter.rest.board
+
+import config.RestdocsTestDsl
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class PostControllerTest : RestdocsTestDsl {
+
+    @Test
+    fun createPost() {
+    }
+
+    @Test
+    fun readPostPageList() {
+    }
+
+    @Test
+    fun readPost() {
+    }
+
+    @Test
+    fun updatePost() {
+    }
+
+    @Test
+    fun deletePost() {
+    }
+}
