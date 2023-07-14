228d5a0505461c9e06b1e8031f0e6cbd17e081e6
hunkicho <jgchk4814@gmail.com>
2023-07-14 10:18:40 +0900

    feat(redis): Set redis
    - Set redis

---
+++ src/main/kotlin/com/example/spring/config/redis/RedisConfig.kt
@@ -0,0 +1,39 @@
+package com.example.spring.config.redis
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") val host: String,
+    @Value("\${spring.data.redis.port}") val port: Int,
+    @Value("\${spring.data.redis.password}") val password: String
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisConfig = RedisStandaloneConfiguration(host, port)
+        redisConfig.password = RedisPassword.of(password)
+
+        return LettuceConnectionFactory(redisConfig)
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<*, *> {
+        return RedisTemplate<Any, Any>().apply {
+            this.connectionFactory = redisConnectionFactory()
+
+            // 콘솔 출력 시 "\xac\xed\x00" 같은 불필요한 해시값을 보지 않기 위해 serializer 설정
+            this.keySerializer = StringRedisSerializer()
+            this.hashKeySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+        }
+    }
+}
