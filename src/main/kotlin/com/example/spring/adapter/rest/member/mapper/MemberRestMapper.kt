38f6681384ba4be68988c2045b56247fabe94311
hunkicho <jgchk4814@gmail.com>
2023-07-17 17:40:31 +0900

    fix(mapper): fix memberRestMapper
    - Add dateFormat

--- src/main/kotlin/com/example/spring/adapter/rest/member/mapper/MemberRestMapper.kt
+++ src/main/kotlin/com/example/spring/adapter/rest/member/mapper/MemberRestMapper.kt
@@ -3,6 +3,8 @@ package com.example.spring.adapter.rest.member.mapper
 import com.example.spring.adapter.rest.member.dto.MemberCommonResponse
 import com.example.spring.domain.member.Member
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.Mappings
 import org.mapstruct.factory.Mappers
 
 @Mapper
@@ -10,6 +12,9 @@ interface MemberRestMapper {
     companion object {
         val INSTANCE: MemberRestMapper = Mappers.getMapper(MemberRestMapper::class.java)
     }
-
+    @Mappings(
+        Mapping(source = "createdAt", target = "createdAt", dateFormat = "yyyy-MM-dd HH:mm:ss"),
+        Mapping(source = "updatedAt", target = "updatedAt", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    )
     fun toMemberCommonResponse(member: Member): MemberCommonResponse
 }
