80b97cbcb438c8c4e291e7c9dced73e17ea93729
hunkicho <jgchk4814@gmail.com>
2023-07-17 17:42:46 +0900

    fix(board): Fix dto
    - Remove useless params

--- src/main/kotlin/com/example/spring/adapter/rest/board/dto/BoardCreateRequest.kt
+++ src/main/kotlin/com/example/spring/adapter/rest/board/dto/BoardCreateRequest.kt
@@ -2,6 +2,5 @@ package com.example.spring.adapter.rest.board.dto
 
 data class BoardCreateRequest(
     val name: String,
-    val description: String,
-    val writer: String
+    val description: String
 )
--- src/main/kotlin/com/example/spring/adapter/rest/board/dto/BoardUpdateRequest.kt
+++ src/main/kotlin/com/example/spring/adapter/rest/board/dto/BoardUpdateRequest.kt
@@ -2,6 +2,5 @@ package com.example.spring.adapter.rest.board.dto
 
 data class BoardUpdateRequest(
     val name: String,
-    val description: String,
-    val modifier: String
+    val description: String
 )
