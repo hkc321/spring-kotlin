7432dbbbe367b3f2030f42d2992a7a2b55793785
hunkicho <jgchk4814@gmail.com>
2023-07-21 15:16:31 +0900

    refactor(file): Remove unused file
    - Remove unused file

--- src/main/kotlin/com/example/spring/config/dto/BaseResponseDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.spring.config.dto
-
-import org.springframework.http.HttpStatus
-
-open class BaseResponseDto {
-    var success: Boolean = true
-    var status: Int = HttpStatus.OK.value()
-}
