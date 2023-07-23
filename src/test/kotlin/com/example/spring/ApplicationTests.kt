ca302135dee60bad373de8187c795474d35a2404
hunkicho <jgchk4814@gmail.com>
2023-07-23 18:31:56 +0900

    test(contextLoad): Add test code
    - Add test code that load context

--- src/test/kotlin/com/example/spring/ApplicationTests.kt
+++ src/test/kotlin/com/example/spring/ApplicationTests.kt
@@ -1,13 +1,24 @@
 package com.example.spring
 
+import io.mockk.*
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.SpringApplication
 
-@SpringBootTest
+
 class ApplicationTests {
 
     @Test
     fun contextLoads() {
+        val someArray = arrayOf("test")
+        mockkStatic(SpringApplication::class)
+
+        every { SpringApplication.run(any()) } returns null
+
+        main(someArray)
+
+        verify { SpringApplication.run(Application::class.java, *someArray) }
+
+        unmockkStatic(SpringApplication::class)
     }
 
 }
