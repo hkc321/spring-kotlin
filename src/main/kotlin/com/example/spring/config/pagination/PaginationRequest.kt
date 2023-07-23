0162b392e4508311242b9bef68a581e26d4aa97f
hunkicho <jgchk4814@gmail.com>
2023-07-23 16:34:30 +0900

    refactor(flie): Remove unused file
    - Remove custom pagination

--- src/main/kotlin/com/example/spring/config/pagination/PaginationRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spring.config.pagination
-
-open class PaginationRequest {
-    var page: Int = 1 // 현재 페이지 번호
-    var recordSize: Int = 5 // 페이지당 출력할 데이터 개수
-    var pageSize: Int = 10 // 화면 하단에 출력할 페이지 사이즈
-}
