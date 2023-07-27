3fc8bc4098fe1103907d2be0a576abbbfd535513
hunkicho <jgchk4814@gmail.com>
2023-07-28 00:16:36 +0900

    feat(comment): Add validation
    - When craete comment, level must over 0

--- src/main/kotlin/com/example/spring/adapter/rest/board/dto/CommentCreateRequest.kt
+++ src/main/kotlin/com/example/spring/adapter/rest/board/dto/CommentCreateRequest.kt
@@ -1,7 +1,10 @@
 package com.example.spring.adapter.rest.board.dto
 
+import jakarta.validation.constraints.Min
+
 data class CommentCreateRequest(
     val parentCommentId: Int? = null,
+    @field: Min(value = 1, message = "level은 1 이상이여야 합니다.")
     val level: Int,
     val content: String
 )
