0cbaf5522577abff0f1a97d0568f907fa1a0ff09
hunkicho <jgchk4814@gmail.com>
2023-07-28 13:35:17 +0900

    feat(comment): Add validation
    - Add validation

--- src/main/kotlin/com/example/spring/adapter/rest/board/dto/CommentCreateRequest.kt
+++ src/main/kotlin/com/example/spring/adapter/rest/board/dto/CommentCreateRequest.kt
@@ -1,10 +1,12 @@
 package com.example.spring.adapter.rest.board.dto
 
+import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 
 data class CommentCreateRequest(
     val parentCommentId: Int? = null,
     @field: Min(value = 1, message = "level은 1 이상이여야 합니다.")
+    @field: Max(value = 2, message = "level은 2 이하여야 합니다.")
     val level: Int,
     val content: String
 )
